59301f43e27f50fafcafcacd19c1bdf2f10c497d
Anna Hetsman <125794946+anna-gt@users.noreply.github.com>
2024-11-26 15:38:22 +0300

    add animations; init jump

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,14 +1,25 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, input, Input, RigidBody2D, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
 export class GameManager extends Component {
-    start() {
 
+	@property(Node)
+	player: Node;
+
+    start() {
+			input.on(Input.EventType.TOUCH_START, this.jump, this)
     }
+
+		private jump() {
+			let body = this.player.getComponent(RigidBody2D);
+			body.linearVelocity = new Vec2(0, 0);
+			body.applyLinearImpulseToCenter(new Vec2(0, 700), true)
+		}
 
     update(deltaTime: number) {
         
     }
 }
-
+
+
